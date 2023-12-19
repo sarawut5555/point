270bb6ec11e458f3b9c76871f44fe74d443f0c86
sarawut <sarawutkai99@gmail.com>
2023-12-19 11:09:52 +0700

    Add project files.

---
+++ point/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace point
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem item1 = new ListViewItem(textBox1.Text);
+            item1.SubItems.Add(textBox2.Text);
+            item1.SubItems.Add(textBox3.Text);
+            item1.SubItems.Add(textBox4.Text);
+            listView1.Items.Add(item1);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count > 0)
+            {
+                listView1.SelectedItems[0].SubItems[0].Text = textBox1.Text;
+                listView1.SelectedItems[0].SubItems[1].Text = textBox2.Text;
+                listView1.SelectedItems[0].SubItems[2].Text = textBox3.Text;
+                listView1.SelectedItems[0].SubItems[3].Text = textBox4.Text;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Remove(listView1.SelectedItems[0]);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+        }
+    }
+}
